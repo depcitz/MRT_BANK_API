172e4484154dd3423169762ac485983d3a872450
Megatron <citinnovation1@hotmail.com>
2020-10-10 10:22:57 +0700

    10/10/2020 10.22

--- apidirectlink.js
+++ apidirectlink.js
@@ -6,6 +6,8 @@ var cors = require('cors')
 const config = require("./config/env");
 const port = config.main_config.PORT
 const pool = require("./config/db_con");
+const util_res_error = require("./utils/util_res_error");
+const code_error = require("./utils/code_error");
 
 
 
@@ -28,6 +30,43 @@ app.use('/n23/status', action_n23_approval_payment);
 app.use('/n24/status', action_n24_approval_payment);
 
 
+
+app.use('/status/approval', (req, res,next) => {
+
+  switch (req.body.comCode) {
+    case "911208":  
+      res.redirect(307,'/n23/status/approval');
+      break;
+    case "911209": 
+      res.redirect(307,'/n24/status/approval');
+      break;
+    default:
+        res.send(util_res_error.json_error_approval(req.body, code_error.status_other_error.respCode, code_error.status_other_error.respMsg))
+      break;
+  }
+}
+)
+
+
+
+app.use('/status/payment', (req, res,next) => {
+
+  switch (req.body.comCode) {
+    case "911208":
+      res.redirect(307,'/n23/status/payment');
+      break;
+    case "911209":   
+      res.redirect(307,'/n24/status/payment');
+      break;
+
+    default:
+        res.send(util_res_error.json_error_payment(req.body, code_error.status_other_error.respCode, code_error.status_other_error.respMsg))
+      break;
+  }
+}
+)
+
+
 // API QRCODE
 const action_n23_qrcode = require('./routes/action_n23_qrcode_route');
 const action_n24_qrcode = require('./routes/action_n24_qrcode_route');
@@ -46,15 +85,15 @@ app.use('/api/pay', action_fastpay);
 
 
 pool.connect().then(client => {
-    return client.query('SELECT NOW()')
-        .then(result => {
-          client.release(true)
-          console.log("connect database  success ")
-        })
-        .catch(err => {
-          client.release(true)
-          return console.error('Error executing query', err.stack)
+  return client.query('SELECT NOW()')
+    .then(result => {
+      client.release(true)
+      console.log("connect database  success ")
+    })
+    .catch(err => {
+      client.release(true)
+      return console.error('Error executing query', err.stack)
 
-        })
+    })
 })
-  
+
--- utils/util_res_error.js
+++ utils/util_res_error.js
@@ -12,9 +12,9 @@ exports.json_error_approval = (obj, respCode, respMsg) => {
                 "balance": 0.00,
                 "cusName": obj.cusName,
                 "info": "information",
-                "print1": "Print 1",
-                "print2": "Print 2",
-                "print3": "Print 3"
+                "print1": "Print 1 approval",
+                "print2": "Print 2 approval",
+                "print3": "Print 3 approval"
         }
 
 }
@@ -32,9 +32,9 @@ exports.json_error_payment = (obj, respCode, respMsg) => {
                 "balance": 0.00,
                 "cusName": cusName,
                 "info": "information",
-                "print1": "Print 1",
-                "print2": "Print 2",
-                "print3": "Print 3"
+                "print1": "Print 1 payment",
+                "print2": "Print 2 payment",
+                "print3": "Print 3 payment"
         }
 
 }
